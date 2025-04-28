badb53faa9e2f323a436c8de57e43d3333f7e0d3
clairecleverlamb <clairechenszeying@gmail.com>
2025-04-28 15:18:12 -0700

    fixed serverAPI

--- src/services/serverAPI.js
+++ src/services/serverAPI.js
@@ -1,2 +1,2 @@
 // export const SERVER_URL = "https://your-heroku-app.herokuapp.com"; 
-export const SERVER_URL = "http://localhost:3000"; 
+export const SERVER_URL = import.meta.env.VITE_SERVER_URL;
