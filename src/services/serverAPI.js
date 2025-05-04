48c7a0fdcf24975e4c96e9f48e2d92185d65a9f2
clairecleverlamb <clairechenszeying@gmail.com>
2025-05-04 13:34:11 -0700

    fix serverApi

--- src/services/serverAPI.js
+++ src/services/serverAPI.js
@@ -1,4 +1,4 @@
 // export const SERVER_URL = "https://your-heroku-app.herokuapp.com"; 
 
 
-export const SERVER_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:3000";
+export const SERVER_URL = import.meta.env.VITE_SERVER_URL || "http://localhost:3000";
