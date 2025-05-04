898f55e1c1e315ce6704a20bd8119a1d6b7fac25
clairecleverlamb <clairechenszeying@gmail.com>
2025-05-04 13:36:08 -0700

    fix serverApi

--- src/services/serverAPI.js
+++ src/services/serverAPI.js
@@ -1,4 +1,5 @@
 // export const SERVER_URL = "https://your-heroku-app.herokuapp.com"; 
 
 
-export const SERVER_URL = import.meta.env.VITE_SERVER_URL || "http://localhost:3000";
+// export const SERVER_URL = import.meta.env.VITE_SERVER_URL || "http://localhost:3000";
+export const SERVER_URL = "https://prayer-board-backend-bcece8a2a0ee.herokuapp.com";
