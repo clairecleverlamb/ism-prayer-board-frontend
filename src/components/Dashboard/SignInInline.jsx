8e112d14dac23b64225093260e93c7168d40b5ba
clairecleverlamb <clairechenszeying@gmail.com>
2025-05-06 16:40:07 -0700

    edit signinline

--- src/components/Dashboard/SignInInline.jsx
+++ src/components/Dashboard/SignInInline.jsx
@@ -1,20 +1,20 @@
 import { Button } from "@/components/ui/button";
-import { SERVER_URL } from "@/services/serverAPI"; 
+import { SERVER_URL } from "@/services/serverAPI";
 
 export default function SignInInline() {
-  const handleGoogleSignIn = () => {
-    window.open = (`${SERVER_URL}/auth/google`,  "_self");
-  };
-
   return (
     <div className="flex flex-col items-center gap-4 mt-10">
-      <Button
-        onClick={handleGoogleSignIn}
-        className="w-64 flex items-center justify-center gap-2"
-        variant="outline"
+      <a
+        href={`${SERVER_URL}/auth/google`}
+        className="w-64" 
       >
-        Sign in with an Acts2 Email
-      </Button>
+        <Button
+          className="w-full flex items-center justify-center gap-2"
+          variant="outline"
+        >
+          Sign in with an Acts2 Email
+        </Button>
+      </a>
     </div>
   );
 }
