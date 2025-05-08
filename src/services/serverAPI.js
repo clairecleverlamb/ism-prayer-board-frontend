3d5c77b3977adabe63eb57008766f285ecf0a785
clairecleverlamb <clairechenszeying@gmail.com>
2025-05-08 00:47:17 -0700

    edit serverApi

--- src/services/serverAPI.js
+++ src/services/serverAPI.js
@@ -2,4 +2,4 @@
 
 // export const SERVER_URL = import.meta.env.VITE_SERVER_URL || "http://localhost:3000";
 // export const SERVER_URL = "https://prayer-board-backend-bcece8a2a0ee.herokuapp.com";
-export const SERVER_URL = "https://ismprayerboard.com";
+export const SERVER_URL = "https://api.ismprayerboard.com";
